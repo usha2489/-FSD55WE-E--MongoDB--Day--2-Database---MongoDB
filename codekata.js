df654bcf17bf77e24773e6a67abd4993c0f4d097
usha2489 <128898127+usha2489@users.noreply.github.com>
2024-09-15 11:54:21 +0530

    Add files via upload

---
+++ codekata.js
@@ -0,0 +1,28 @@
+[{
+    "user_name":"fabian",
+    "problems_solved":88
+},
+{
+    "user_name":"raja",
+    "problems_solved":18
+},
+{
+    "user_name":"fernando",
+    "problems_solved":48
+},
+{
+    "user_name":"rohit",
+    "problems_solved":58
+},
+{
+    "user_name":"virat",
+    "problems_solved":45
+},
+{
+    "user_name":"dhawan",
+    "problems_solved":91
+},
+{
+    "user_name":"dhoni",
+    "problems_solved":11
+}]
